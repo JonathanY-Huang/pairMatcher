9c6e2aabd4543901babc5ba64b5f1270b052267d
Jonathan Huang <32203695+JonathanY-Huang@users.noreply.github.com>
2018-08-30 17:36:31 -0400

    Added correct file
    also added comments

---
+++ pairMatcher.py
@@ -0,0 +1,48 @@
+import os.path
+import pickle
+import random
+
+if not os.path.exists("students.txt"):
+	print("Please add a list of students!")
+else:
+	studentList = open("students.txt", "r")
+	pairs = open("pairs.txt", "w")
+
+	students = []
+	for line in studentList:
+		students.append(line.rstrip("\n"))
+
+	#Loads previous matching data 
+	try:
+		prevMatched = pickle.load(open("prevCombinations.p","rb"))
+	except:
+		prevMatched = {}
+
+	#Matches the pairs
+	while len(students) > len(students) % 2:
+		first = random.choice(students)
+		students.remove(first)
+
+		second = ""
+		unique = False
+		while not unique: #ensures a pair is not repeated
+			second = random.choice(students)
+			try:
+				if prevMatched[first] != second:
+					unique = True
+			except:
+				unique = True
+					
+		students.remove(second)
+
+		prevMatched[first] = second
+		prevMatched[second] = first
+		
+		pairs.write('{} - {}\n'.format(first, second))
+
+	pairs.write(students[0])
+
+	#Saves the matching data
+	pickle.dump(prevMatched, open("prevCombinations.p", "wb"))
+	
+
